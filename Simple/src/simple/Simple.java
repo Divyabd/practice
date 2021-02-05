56bcef4c00944e5ebddb6483e9aaac5ebb024c0a
Divyabd <divyagowda8393@gmail.com>
2021-02-05 17:34:05 +0530

    master commit

---
+++ Simple/src/simple/Simple.java
@@ -0,0 +1,12 @@
+package simple;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int m=1;
+		int s=2;
+		System.out.println("hello ppls");
+	}
+
+}
